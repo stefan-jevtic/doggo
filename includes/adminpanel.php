0055beeaa9012b6220de06e044571b38acdeb118
stefan_jevtic <sjevtic14@gmail.com>
2018-01-16 23:22:58 +0100

    started working on admin panel

---
+++ includes/adminpanel.php
@@ -0,0 +1,220 @@
+<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+  <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+  <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
+  <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
+  <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+</div>
+<div class="tab-content" id="v-pills-tabContent">
+  <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+  <div class="panel panel-success" style="border: 1px solid #ddd; border-radius: 5px;">
+      <div class="alert alert-success panel-heading">
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-3">
+            <h2 class="text-center pull-left" style="padding-left: 30px;"> <span class="glyphicon glyphicon-list-alt"> </span> Users </h2>
+          </div>
+          <!-- <div class="col-xs-9 col-sm-9 col-md-9">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+              <div class="col-xs-12 col-md-4">
+                <label> Search </label>
+                <div class="form-group">
+                  <div class="input-group">
+                    <input type="text" class="form-control input-md" name="search">
+                    <div class="input-group-btn">
+                      <button type="button" class="btn btn-md btn-warning"> <span class=" glyphicon glyphicon-search"></span></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> -->
+        </div>
+      </div>
+
+      <div class="panel-body table-responsive">
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th class="text-center"> No. </th>
+              <th class="text-center"> Name </th>
+              <th class="text-center"> Mobile No. </th>
+              <th class="text-center"> E-Mail </th>
+              <th class="text-center"> City </th>
+              <th class="text-center">Update</th>
+              <th class="text-center">Delete</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr class="edit" id="detail">
+              <td id="no" class="text-center"> 1 </td>
+              <td id="name" class="text-center"> ABC </td>
+              <td id="mobile" class="text-center"> 5412547854 </td>
+              <td id="mail" class="text-center"> abc@gmail.com </td>
+              <td id="city" class="text-center"> New York </td>
+              <td class="text-center"><button class="btn btn-primary">Change</button></td>
+              <td class="text-center"><button class="btn btn-danger">Delete</button></td>
+            </tr>
+
+            <tr>
+              <td class="text-center"> 2 </td>
+              <td class="text-center"> DFG </td>
+              <td class="text-center"> 4025478965 </td>
+              <td class="text-center"> dfg@gmail.com </td>
+              <td class="text-center"> Los Angelos </td>
+              <td class="text-center"><button class="btn btn-primary">Change</button></td>
+              <td class="text-center"><button class="btn btn-danger">Delete</button></td>
+            </tr>
+
+            <tr>
+              <td class="text-center"> 3 </td>
+              <td class="text-center"> XYZ </td>
+              <td class="text-center"> 4102369745 </td>
+              <td class="text-center"> xyz@gmail.com </td>
+              <td class="text-center"> Las Vegas </td>
+              <td class="text-center"><button class="btn btn-primary">Change</button></td>
+              <td class="text-center"><button class="btn btn-danger">Delete</button></td>
+            </tr>
+
+            <tr>
+              <td class="text-center"> 4 </td>
+              <td class="text-center"> JKL </td>
+              <td class="text-center"> 2536541028 </td>
+              <td class="text-center"> jkl@gmail.com </td>
+              <td class="text-center"> California </td>
+              <td class="text-center"><button class="btn btn-primary">Change</button></td>
+              <td class="text-center"><button class="btn btn-danger">Delete</button></td>
+            </tr>
+
+            <tr>
+              <td class="text-center"> 5 </td>
+              <td class="text-center"> DFG </td>
+              <td class="text-center"> 4025478965 </td>
+              <td class="text-center"> dfg@gmail.com </td>
+              <td class="text-center"> Los Angelos </td>
+              <td class="text-center"><button class="btn btn-primary">Change</button></td>
+              <td class="text-center"><button class="btn btn-danger">Delete</button></td>
+            </tr>
+            
+          </tbody>
+        </table>
+      </div>
+
+      <div class="panel-footer">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="col-md-8">
+              </div>
+              <div class="col-md-4">
+              <p class="muted pull-right"><strong> © 2018 All rights reserved </strong></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div></div>
+<div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab"><div class="row">
+        <div class="panel panel-default widget">
+            <div class="panel-heading">
+                <span class="glyphicon glyphicon-comment"></span>
+                <h3 class="panel-title">
+                    Recent Comments</h3>
+                <span class="label label-info">
+                    78</span>
+            </div>
+            <div class="panel-body">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <div class="row">
+                            <div class="col-xs-2 col-md-1">
+                                <img src="http://placehold.it/80" class="rounded-circle img-responsive" alt="" /></div>
+                            <div class="col-xs-10 col-md-11">
+                                <div>
+                                    <a href="http://www.jquery2dotnet.com/2013/10/google-style-login-page-desing-usign.html">
+                                        Google Style Login Page Design Using Bootstrap</a>
+                                    <div class="mic-info">
+                                        By: <a href="#">Bhaumik Patel</a> on 2 Aug 2013
+                                    </div>
+                                </div>
+                                <div class="comment-text">
+                                    Awesome design
+                                </div>
+                                <div class="action">
+                                    <button type="button" class="btn btn-primary btn-xs" title="Edit">
+                                        <span class="glyphicon glyphicon-pencil"></span>
+                                    </button>
+                                    <button type="button" class="btn btn-success btn-xs" title="Approved">
+                                        <span class="glyphicon glyphicon-ok"></span>
+                                    </button>
+                                    <button type="button" class="btn btn-danger btn-xs" title="Delete">
+                                        <span class="glyphicon glyphicon-trash"></span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">
+                        <div class="row">
+                            <div class="col-xs-2 col-md-1">
+                                <img src="http://placehold.it/80" class="rounded-circle img-responsive" alt="" /></div>
+                            <div class="col-xs-10 col-md-11">
+                                <div>
+                                    <a href="http://bootsnipp.com/BhaumikPatel/snippets/Obgj">Admin Panel Quick Shortcuts</a>
+                                    <div class="mic-info">
+                                        By: <a href="#">Bhaumik Patel</a> on 11 Nov 2013
+                                    </div>
+                                </div>
+                                <div class="comment-text">
+                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+                                    euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim
+                                </div>
+                                <div class="action">
+                                    <button type="button" class="btn btn-primary btn-xs" title="Edit">
+                                        <span class="glyphicon glyphicon-pencil"></span>
+                                    </button>
+                                    <button type="button" class="btn btn-success btn-xs" title="Approved">
+                                        <span class="glyphicon glyphicon-ok"></span>
+                                    </button>
+                                    <button type="button" class="btn btn-danger btn-xs" title="Delete">
+                                        <span class="glyphicon glyphicon-trash"></span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">
+                        <div class="row">
+                            <div class="col-xs-2 col-md-1">
+                                <img src="http://placehold.it/80" class="rounded-circle img-responsive" alt="" /></div>
+                            <div class="col-xs-10 col-md-11">
+                                <div>
+                                    <a href="http://bootsnipp.com/BhaumikPatel/snippets/4ldn">Cool Sign Up</a>
+                                    <div class="mic-info">
+                                        By: <a href="#">Bhaumik Patel</a> on 11 Nov 2013
+                                    </div>
+                                </div>
+                                <div class="comment-text">
+                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+                                    euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim
+                                </div>
+                                <div class="action">
+                                    <button type="button" class="btn btn-primary btn-xs" title="Edit">
+                                        <span class="glyphicon glyphicon-pencil"></span>
+                                    </button>
+                                    <button type="button" class="btn btn-success btn-xs" title="Approved">
+                                        <span class="glyphicon glyphicon-ok"></span>
+                                    </button>
+                                    <button type="button" class="btn btn-danger btn-xs" title="Delete">
+                                        <span class="glyphicon glyphicon-trash"></span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <a href="#" class="btn btn-primary btn-sm btn-block" role="button"><span class="glyphicon glyphicon-refresh"></span> More</a>
+            </div>
+        </div>
+    </div>
+</div>
+  <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">dsaaaaaaaaaaaa</div>
+  <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+</div>
