c62651de3bbe24b55061b0b3795d7cb440262bad
stefan_jevtic <sjevtic14@gmail.com>
2018-01-23 23:47:38 +0100

    explore feed started

---
+++ includes/explorefeed.php
@@ -0,0 +1,33 @@
+<div class="row justify-content-md-center">
+    <div class="col-md-6">
+        <div class="story-container">
+            <div class="image-container">
+
+            </div>
+            <div class="actions">
+                <div class="statistics">
+                    <small class="form-text text-muted likes-counter">10 likes</small>
+                    <small class="form-text text-muted dislikes-counter">3 dislikes</small>
+                    <small class="form-text text-muted comments-counter">42 comments</small>
+                </div>
+                <div class="buttons">
+                    <span class="like-button"><i class="far fa-thumbs-up"></i></span>
+                    <span class="dislike-button"><i class="far fa-thumbs-down"></i></span>
+                    <span class="comment-button"><i class="far fa-comments"></i></span>
+                </div>
+                <div class="insert-comment">
+                    <textarea name="comment" id="comment" cols="15" rows="2" class="form-control"></textarea>
+                </div>
+            </div>
+            <div class="list-of-comments">
+                <div class="each-comment">
+                    <span class="user-image"><img src="" alt=""></span>
+                    <span class="user-name"><b>kica</b></span>
+                    <small class="form-test text-muted date-commented">2018-01-23 22:58</small>
+                    <div class="user-message"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, error! Alias sunt autem perspiciatis ea. Distinctio ipsum totam asperiores doloribus iure laboriosam corrupti blanditiis voluptatum, velit ad corporis, delectus labore?</p></div>
+                </div>
+                <a href="#"><small class="form-test text-muted">Load more comments</small></a>
+            </div>
+        </div>
+    </div>
+</div>
